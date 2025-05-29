b046af59f34eebe562bb7839e965a438ad30e242
sashanclrp <sasha@candyflip.co>
2025-05-29 15:16:35 -0500

    [DEPLOY] Deploy the First version of Mimeiapify

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_long_description():
     return ""
 
 setup(
-    name='sncl',  # Your package name
+    name='mimeiapify',  # Your package name
     version='1.2.0',  # Version of your package
     packages=find_packages(),  # Automatically find all packages in your project
     install_requires=[
@@ -19,17 +19,17 @@ setup(
         'aiohttp',
         'asyncio'
     ],
-    description="A utility library for API interactions including Wompi payments and Airtable",  # Short description
+    description="A utility library for Symphony AI",  # Short description
     long_description=read_long_description(),  # Use README.md as the long description
     long_description_content_type="text/markdown",  # Specify Markdown format for the README
     author='Sasha Nicolai Canal',  # Update with your name
-    author_email='sasha@candyflip.co',  # Update with your email
-    url='https://github.com/sashanclrp/sncl-personal-libraries',  # Replace with your GitHub repository
+    author_email='contact@mimeia.com',  # Update with your email
+    url='https://github.com/mimeia-ai/mimeiapify',  # Replace with your GitHub repository
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",  # Update if not using MIT
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  # Minimum Python version requirement
+    python_requires='>=3.8',  # Minimum Python version requirement
     license="MIT",  # Update if not using MIT
 )
--- sncl/__init__.py
+++
@@ -1,7 +0,0 @@
-# sncl/__init__.py
-
-from .airtable import Airtable  # Import synchronous Airtable
-from .airtable_async import AirtableAsync  # Import asynchronous Airtable
-from .wompi_async import WompiAsync  # Import asynchronous Wompi
-
-__all__ = ["Airtable", "AirtableAsync", "WompiAsync"]
